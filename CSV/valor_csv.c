6fc63a0dbaa616f4b89353bc4f24da64d8d21702
vep20 <vep20@inf.ufpr.br>
2026-03-11 17:31:24 -0300

    Commit automático

--- CSV/valor_csv.c
+++ CSV/valor_csv.c
@@ -5,16 +5,27 @@
 int main (){
     char csv[101];
     char *token;// ponteiro auxiliar para separar colunas
+    int cont;
 
-    scanf("%s", csv);
-    printf ("Quantidade caracteres digitados: %ld\n", strlen(csv));
-    // Obs: função strlen retorna long int 
-    token = strtok (csv, ";"); // separa a string em substrings com final NULL
-    while (token){
-        printf (" %ld", strlen(token));
-        token = strtok(NULL, ";");// Modo de fazer continuar na mesma string passada anteriormente
+    //scanf("%s", csv);
+
+    // le uma string do "teclado" (stdin) do tamanho de uma linha armazenada em csv
+    // adiciona um '/n' no final desta string 
+    cont = 0; 
+    while (fgets (csv, sizeof (csv), stdin)){
+        cont++;
+        //troca o \n que o fgets deix para um \0
+        csv[strcspn  (csv, "\n")] = '\0';
+
+        printf ("linha %d quantidade caracteres digitados: %ld\n",cont, strlen(csv));
+        // Obs: função strlen retorna long int 
+        token = strtok (csv, ";"); // separa a string em substrings com final NULL
+        printf("Quantidade caracteres por colunas:");
+        while (token){
+            printf (" %ld", strlen(token));
+            token = strtok(NULL, ";");// Modo de fazer continuar na mesma string passada anteriormente
+        }
+        printf ("\n\n");
     }
-    printf ("\n");
     return 0;
 }
-
