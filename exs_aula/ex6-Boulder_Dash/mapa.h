00f279c857c40a79f45bc5074b97cb5a40be3152
vep20 <vep20@inf.ufpr.br>
2026-03-30 18:59:01 -0300

    Commit automático

---
+++ exs_aula/ex6-Boulder_Dash/mapa.h
@@ -0,0 +1,16 @@
+// Biblioteca para carregar arquivos txt em matrizes de caracteres
+
+#ifndef MATRIZ
+#define MATRIZ
+
+
+//função que retorna altura do mapa
+
+//função que retorna largula do mapa
+
+//função que retornar o numero de celulas do mapa
+
+//função que retorna a matriz do mapa
+
+
+#endif 
