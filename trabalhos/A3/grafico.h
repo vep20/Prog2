90c5021ccab6cc189455a5d97660b7e5d3eaf13d
vep20 <vep20@inf.ufpr.br>
2026-06-14 21:46:29 -0300

    Commit automático

--- trabalhos/A3/grafico.h
+++ trabalhos/A3/grafico.h
@@ -1,6 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef GRAFICO_H
+#define GRAFICO_H
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>      // Para imagens
 #include <allegro5/allegro_primitives.h> // Para formas geométricas
 #include <allegro5/allegro_font.h>       // Para textos
+#include <stdbool.h>
+
+// constantes do jogo 
+#define TELA_LARGURA 800
+#define TELA_ALTURA 60
+#define FPS 60.0 
+
+// lista de nomes representandos inteiros para as acoes do personagem
+enum ACOES {
+    EM_PE, 
+    ANDANDO,
+    PULANDO,
+    ABAIXANDO, 
+    INTERAGINDO
+};
+
+#endif
--- trabalhos/A3/tarzan_federal.c
+++ trabalhos/A3/tarzan_federal.c
@@ -1,7 +1,9 @@
 #include "grafico.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main (){
-
+    
     
     return 0;
 }
